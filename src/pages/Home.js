a7027189606c8f39fb169ab099429bc4eacd8c4c
Davids <75976997+KrasnovsD98@users.noreply.github.com>
2023-11-05 20:05:35 +0200

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,28 +2,30 @@ import Header from './../components/header/Header'
 
 const Home = () => {
     return (
-		<>
-			<Header />
-			<main className="section">
-				<div className="container">
-					<ul className="content-list">
-						<li className="content-list__item">
-							<h2 className="title-2">Frontend</h2>
-							<p>
-								JavaScript, ReactJS ,
-								HTML, CSS, BootStrap,
-								TailwindCSS, SCSS
-							</p>
-						</li>
-						<li className="content-list__item">
-							<h2 className="title-2">Backend</h2>
-							<p>NodeJS, MySQL, MongoDB, PHP, C#, .NET</p>
-						</li>
-					</ul>
-				</div>
-			</main>
-		</>
-	);
+      <>
+        <Header />
+        <main className="section">
+          <div className="container">
+            <ul className="content-list">
+              <li className="content-list__item">
+                <h2 className="title-2">Frontend</h2>
+                <p>
+                  JavaScript, ReactJS , HTML, CSS, BootStrap, TailwindCSS, SCSS
+                </p>
+              </li>
+              <li className="content-list__item">
+                <h2 className="title-2">Backend</h2>
+                <p>NodeJS, MySQL, PHP, C#, .NET</p>
+              </li>
+              <li className="content-list__item">
+                <h2 className="title-3">Cloud computing platform</h2>
+                <p>ServiceNow</p>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </>
+    );
 }
 
 export default Home;
