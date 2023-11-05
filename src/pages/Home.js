7697520979c0fc1f0f006c6c9dae9aa3b13cc0bf
Davids <75976997+KrasnovsD98@users.noreply.github.com>
2023-11-05 20:56:00 +0200

    Add files via upload
    update titles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,15 @@ const Home = () => {
               </li>
               <li className="content-list__item">
                 <h2 className="title-2">Backend</h2>
-                <p>ServiceNow, NodeJS, MySQL, PHP, C#, .NET</p>
+                <p>NodeJS, MySQL, PHP, C#, .NET</p>
+              </li>
+              <li className="content-list__item">
+                <h3 className="title-2">Cloud computing platform</h3>
+                <p>ServiceNow</p>
+              </li>
+              <li className="content-list__item">
+                <h4 className="title-2">Scrum</h4>
+                <p>Agile Methodology, SAFE</p>
               </li>
             </ul>
           </div>
