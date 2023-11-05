246473631c5fed3d9a69bcf817c65ed6f42ba43c
Davids <75976997+KrasnovsD98@users.noreply.github.com>
2023-11-05 20:06:30 +0200

    Add files via upload

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,25 +1,25 @@
-import project02 from "./../img/projects/04.png";
+import project02 from "./../img/projects/01.png";
 import project02Big from "./../img/projects/04-big.png";
 
-import project03 from "./../img/projects/05.png";
+import project03 from "./../img/projects/02.png";
 import project03Big from "./../img/projects/05-big.png";
 
 
 const projects = [
-	{
-		title: 'Music App',
-		img: project02,
-		imgBig: project02Big,
-		skills: 'React, Netify',
-		gitHubLink: 'https://github.com',
-	},
-	{
-		title: 'Expensies tracker',
-		img: project03,
-		imgBig: project03Big,
-		skills: 'React JS, Node.js',
-		gitHubLink: 'https://github.com',
-	},
+  {
+    title: "Music App",
+    img: project02,
+    imgBig: project02Big,
+    skills: "React JS, Node.js, Netify",
+    gitHubLink: "https://github.com/KrasnovsD98/Music",
+  },
+  {
+    title: "Expenses tracker",
+    img: project03,
+    imgBig: project03Big,
+    skills: "React JS, Node.js",
+    gitHubLink: "https://github.com/KrasnovsD98/Expenses",
+  },
 ];
 
 export {projects}
