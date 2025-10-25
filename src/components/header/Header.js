b693fdb8385a1d983c214c3df247e0c5790ffd81
Davids <davids.krasnovs98@gmail.com>
2025-10-25 23:46:57 +0300

    cv link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
           <motion.a
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }} 
-            href="https://drive.google.com/file/d/1zm96shzXInldICU6CVQRcZGVovvOGgWD/view?usp=drive_link"
+            href="https://drive.google.com/file/d/14zvQgpbf4eMs6-Pv5IhW_muhxxSw4LIF/view?usp=sharing"
             className="btn"
           >
             Download CV
